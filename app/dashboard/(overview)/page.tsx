4a6e733725e7d7a1dfa40b91969023b064fe9ca8
Artur Mostowski <artur.mostowski@protonmail.com>
2024-05-25 09:57:57 +0200

    more suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,9 +6,8 @@ import {
   fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import {LatestInvoicesSkeleton, RevenueChartSkeleton} from '@/app/ui/skeletons';
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -35,7 +34,9 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
